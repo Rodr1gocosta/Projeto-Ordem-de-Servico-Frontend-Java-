e3c076a32e701468857d78756a5ec89c1d2ccb29
Rodrigo Costa <rodrigomcosta20@gmail.com>
2023-08-22 20:35:16 -0300

    Update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://os-servico.herokuapp.com'
-  //baseUrl: 'http://localhost:8080'
+  // baseUrl: 'https://os-servico.herokuapp.com'
+  baseUrl: 'http://localhost:8081'
 
 };
 
